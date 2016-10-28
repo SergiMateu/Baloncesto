3871f044d1e1fcdeb4fc8fcc86d9de42ac7a2d54
SergiMateu <sergisnk@gmail.com>
2016-10-28 18:01:49 +0200

    Controller 1

---
+++ src/main/java/com/example/controller/EquipoController.java
@@ -0,0 +1,46 @@
+package com.example.controller;
+
+import com.example.domain.Equipo;
+import com.example.repository.EquipoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by 46422520x on 24/10/2016.
+ */
+@RestController
+@RequestMapping ("/equipos")
+public class EquipoController {
+
+    @Autowired
+    private EquipoRepository equipoRepository;
+
+    public EquipoController(){
+
+    }
+
+    @RequestMapping(method = {RequestMethod.POST})
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Equipo createEquipo(@RequestBody Equipo equipo){return(Equipo)this.equipoRepository.save(equipo);}
+
+    @RequestMapping(method = {RequestMethod.GET})
+    public List<Equipo> findAll(){return this.equipoRepository.findAll();}
+
+    @RequestMapping(value = "/{id}",
+            method = {RequestMethod.GET})
+    public Equipo findById(@PathVariable Long id){
+        Equipo equipo =  (Equipo)this.equipoRepository.findOne(id);
+        return equipo;
+    }
+
+    @RequestMapping(value = "/byLocalidad/{local}",
+            method = RequestMethod.GET)
+    public List<Equipo> findByLocalidad(@PathVariable String local){
+        return equipoRepository.findByLocalidad(local);
+    }
+
+}
---
+++ src/main/java/com/example/controller/JugadorController.java
@@ -0,0 +1,46 @@
+package com.example.controller;
+
+import com.example.domain.Jugador;
+import com.example.repository.JugadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by 46422520x on 24/10/2016.
+ */
+@RestController
+@RequestMapping ("/jugadores")
+public class JugadorController {
+
+    @Autowired
+    private JugadorRepository jugadorRepository;
+
+    public JugadorController(){
+
+    }
+
+    @RequestMapping(method = {RequestMethod.POST})
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Jugador createJugador(@RequestBody Jugador jugador){return(Jugador)this.jugadorRepository.save(jugador);}
+
+    @RequestMapping(method = {RequestMethod.GET})
+    public List<Jugador> findAll(){return this.jugadorRepository.findAll();}
+
+    @RequestMapping(value = "/{id}",
+            method = {RequestMethod.GET})
+    public Jugador findById(@PathVariable Long id){
+        Jugador jugador =  (Jugador)this.jugadorRepository.findOne(id);
+        return jugador;
+    }
+
+    @RequestMapping(value = "/byCanastas/{num}",
+            method = RequestMethod.GET)
+    public List<Jugador> findByPointsGreaterThan(@PathVariable Integer num){
+        return jugadorRepository.findBycanastasGreaterThanEqual(num);
+    }
+
+}
